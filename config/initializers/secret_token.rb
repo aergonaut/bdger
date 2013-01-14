44177989c037d3d2fe79e23eba428e912f71c8a1
Chris Fung <aergonaut@gmail.com>
2013-01-14 11:23:23 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bdger::Application.config.secret_token = '0657a6e7e716413c48667cf2dc46fe6931c56cff3bcbed591729d1153bc8f35ec1d2715a3ab812b17a206337c23021171f230e542952f7e4cebeba6564844364'
