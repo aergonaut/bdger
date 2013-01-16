f9eab84b5b0c76a4480ccb6ab431ebf2b0e9b0be
Chris Fung <aergonaut@gmail.com>
2013-01-15 19:19:26 -0800

    added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Bdger::Application.routes.draw do
   devise_for :users
 
-  root to: "badges#list"
+  root to: "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
