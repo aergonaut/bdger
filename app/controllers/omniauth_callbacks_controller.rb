1070ec07dee82f09a865c09e182d03fe21759e33
Chris Fung <aergonaut@gmail.com>
2013-01-16 00:50:37 -0800

    implemented basic auth with twitter

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,11 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def all
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      sign_in_and_redirect user, notice: "Signed in!"
+    else
+      redirect_to root
+    end
+  end
+  alias_method :twitter, :all
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Bdger::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
   root to: "home#index"
 
