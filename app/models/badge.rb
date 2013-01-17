70e7b77013ca2a5ce46ee93e7ccdeef48dcb5c16
Chris Fung <aergonaut@gmail.com>
2013-01-17 01:48:25 -0800

    added basic badge model

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,3 +1,4 @@
 class Badge < ActiveRecord::Base
   attr_accessible :big_cartel_link, :description, :image, :name, :tumblr_link
+  validates :name, presence: true
 end
--- spec/models/badge_spec.rb
+++ spec/models/badge_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Badge do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @badge = Badge.new name: "French Language", description: "Learn French",
+      tumblr_link: "http://lifescouts.com/post/39558983039/lifescouts-french-language-badge-if-you-have",
+      big_cartel_link: "http://lifescouts.bigcartel.com/product/lifescouts-french-badge",
+      image: "x.png"
+  end
+
+  subject { @badge }
+
+  describe "when name is empty" do
+    before { @badge.name = "" }
+    it { should_not be_valid }
+  end
 end
