04fa603e74b7471e395d5bba8bbd13259690a058
Chris Fung <aergonaut@gmail.com>
2013-01-22 08:17:34 -0800

    users#show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe UsersController do
 
+  describe "GET 'show'" do
+    before :each do
+      @user = FactoryGirl.create :user
+    end
+
+    it "should be successful" do
+      get :show, username: @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, username: @user
+      assigns(:user).should == @user
+    end
+  end
+
 end
