94320dfe9e1ef9ee2e07d35547ff375510fbae7c
Chris Fung <aergonaut@gmail.com>
2013-01-21 22:59:47 -0800

    remove extra devise modules; use only omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :remember_me, :username
   # attr_accessible :title, :body
+
+  extend FriendlyId
+  friendly_id :username, use: :slugged
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
@@ -15,13 +17,5 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.username = auth.info.nickname
     end
-  end
-
-  def password_required?
-    super && provider.blank?
-  end
-
-  def email_required?
-    super && provider.blank?
   end
 end
