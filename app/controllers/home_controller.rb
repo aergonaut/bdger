b4d602f99c88878ce82e9e818ac4adce848e7d9b
Chris Fung <aergonaut@gmail.com>
2013-01-26 22:38:27 -0800

    redirect root to badges if user signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
-    render 'index', layout: 'root'
+    if user_signed_in?
+      redirect_to badges_path
+    else
+      render 'index', layout: 'root'
+    end
   end
 
   def privacy
