a179956a797fdcf4a2d173e73960aaa4400274dc
Chris Fung <aergonaut@gmail.com>
2013-01-30 01:11:45 -0800

    tweak devise

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Bdger::Application.routes.draw do
     get "sign_in", to: "devise/sessions#new", as: :new_user_session
     get "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
   end
-  get "sign_in", to: "devise/sessions#new", as: :new_session
-  get "sign_out", to: "devise/sessions#destroy", as: :destroy_session
 
   # badges
   resources :badges
